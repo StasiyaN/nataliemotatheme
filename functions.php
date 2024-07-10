b42dadca0008285f6154b0905edec937a31db7fa
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-10 12:56:33 +0200

    quelques tests functions.php menus ok, le reste en cours

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+//creation des massives avec les parametres pour les add_theme_support
+// $nmheader = [
+
+// ];
+
+// MENUS
+add_action('after_setup_theme', function() {
+    add_theme_support('menus');
+});
+
+
+
