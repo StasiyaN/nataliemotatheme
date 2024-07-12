08142da676c7cede9e044b6ee92c62f9cffba140
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-12 23:01:16 +0200

    creation de la fenetre popup en cours

---
+++ template-parts/contact-popup.php
@@ -0,0 +1,6 @@
+<div class="contact">
+    <div class="contact-content">
+        <img src="<?php echo get_template_directory_uri () . '/assets/img/contact-header.png';?>" alt="" class="popup-header">
+    
+    </div>
+</div>
