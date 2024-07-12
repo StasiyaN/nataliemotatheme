bbd6898cbc51a6f884f0c5d94e1dacbc0ee6c99c
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-12 17:20:39 +0200

    suppression partie banniere du front-page

--- front-page.php
+++ front-page.php
@@ -1,9 +1,7 @@
 <?php
 get_header(); ?>
 
-<!-- <div class="banner">
-    <img src="<?php echo get_template_directory_uri() . '/assets/img/header.jpg'; ?>" alt="banner du site" class = "banner-img">
-</div> -->
+
 
 <?php
 get_footer();
