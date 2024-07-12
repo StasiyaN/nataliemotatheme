7a81e5a617333c63f7166c42979afab7205d1f55
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-12 08:06:41 +0200

    modification styles nav / ajout de quelques styles genarales / home page travail en cours

--- home-page.php
+++ home-page.php
@@ -2,7 +2,12 @@
 /*
 Template Name: Home Page Template
 */
-get_header();
+get_header(); ?>
 
+<div class="banner">
+    <img src="<?php echo get_template_directory_uri() . '/assets/img/header.jpg'; ?>" alt="banner du site" class = "banner-img">
+</div>
+
+<?php
 get_footer();
 ?>
