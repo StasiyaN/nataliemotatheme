cc777db80a9006ff17c62dd686e34db9785000c4
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-13 17:27:36 +0200

    changement de noms de qq variables de couleur / css contact-popup en cours

--- front-page.php
+++ front-page.php
@@ -1,6 +1,8 @@
 <?php
-get_header(); ?>
+get_header(); 
 
+get_template_part('/template-parts/contact-popup'); 
+?>
 
 
 <?php
--- template-parts/contact-popup.php
+++ template-parts/contact-popup.php
@@ -1,6 +1,8 @@
-<div class="contact">
-    <div class="contact-content">
-        <img src="<?php echo get_template_directory_uri () . '/assets/img/contact-header.png';?>" alt="" class="popup-header">
-    
+<div class="contact-popup">
+    <div class="popup-wrapper">
+            <img src="<?php echo get_template_directory_uri () . '/assets/img/contact-header.png';?>" alt="" class="popup-header">
+        <div class="popup-form">
+            <?php echo apply_shortcodes( '[contact-form-7 id="5db65a9" title="Contact popup"]' ); ?>
+        </div>
     </div>
 </div>
