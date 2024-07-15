d1450d7f4027a6f4451befef75fa6952db878355
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-15 20:25:02 +0200

    script popup ok

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,4 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log('script loaded');
+    //creation du scri^pt pout le popup
+    //declaration des variables
+    let contactBtn = document.getElementById('contactBtn');
+    let popupWindow = document.querySelector('.contact-popup');
+    let popupOverlay = document.createElement('div');
 
+    popupOverlay.classList.add('popup-overlay');
+    document.body.appendChild(popupOverlay);
+    console.log(contactBtn);
+    console.log(popupWindow);
+    console.log(popupOverlay);
+    //ajout du eventlistent on click
+
+    if (!contactBtn) {
+        console.error('Contact button not found');
+        return;
+    }
+    if (!popupWindow) {
+        console.error('Popup window not found');
+        return;
+    }
+
+    contactBtn.addEventListener('click', function () {
+        popupWindow.classList.add('active');
+        popupOverlay.classList.add('active');
+      
+       
+    });
+
+    popupOverlay.addEventListener('click', function () {
+        popupWindow.classList.remove('active');
+        popupOverlay.classList.remove('active');
+    });
 });
