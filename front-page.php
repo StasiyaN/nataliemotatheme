13051d5cb4e2f4c2ac8e1cfcca80df67c10cf10c
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-24 10:14:38 +0200

    ajout de labels pour le tri dans front page

--- front-page.php
+++ front-page.php
@@ -6,6 +6,35 @@ get_header();
         <h1>PHOTOGRAPHE EVENT</h1>
     </div>
     <div class="page-body">
+        <div class="filters">
+            <label for="categorie"></label>
+                <select id="categorie" name="categorie">
+                    <option value="">Catégories</option>
+                    <?php
+                    $categories = get_terms('categories'); // Adjust taxonomy name if needed
+                    foreach ($categories as $category) {
+                        echo '<option value="' . esc_attr($categorie->slug) . '">' . esc_html($catgorie->name) . '</option>';
+                    }
+                    ?>
+                </select>
+            <label for="format"></label>
+                <select id="format" name="format">
+                    <option value="">Formats</option>
+                    <?php
+                    $formats = get_terms('formats'); // Adjust taxonomy name if needed
+                    foreach ($formats as $format) {
+                        echo '<option value="' . esc_attr($format->slug) . '">' . esc_html($format->name) . '</option>';
+                    }
+                    ?>
+                </select>
+                <label for="sort"></label>
+                    <select id="sort" name="sort">
+                        <option value="desc">Trier par</option>
+                        <option value="desc">Plus récentes</option>
+                        <option value="asc">Plus anciennes</option>
+                    </select>
+
+        </div>
 
         <?php
         // Define WP Query parameters
