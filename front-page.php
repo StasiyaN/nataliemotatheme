8975922fddf87bb401360323c8e9c677eb0103c5
StasiyaN <ana.nyzhalska@gmail.com>
2024-07-29 17:32:46 +0200

    banniere desktop ok

--- front-page.php
+++ front-page.php
@@ -1,8 +1,30 @@
 <?php get_header();?>
+
 <div class="page-content">
+    <!-- custom wp_query function pour recuperer les parametres des photos en aleatoire -->
+    <?php 
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 1,
+        'orderby' => 'rand'
+    );
+    
+    $banner_image_query = new WP_Query($args);
+
+    if ($banner_image_query->have_posts()) {
+        while ($banner_image_query->have_posts()) {
+            $banner_image_query->the_post();
+            $image_url = get_the_post_thumbnail_url(get_the_ID(), 'banner');
+        }
+        wp_reset_postdata();
+    }
+    ?>
+
     <div class="banner">
+        <img src="<?php echo esc_url($image_url); ?>" alt="banniere du site" class="banner-img">
         <h1>PHOTOGRAPHE EVENT</h1> 
     </div> 
 </div>
 
-<?php get_footer();?>
+    
+ <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -16,13 +16,8 @@ function nataliemota_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'nataliemota_enqueue_scripts');
 
-// getting rid of other styles
-// function remove_wp_block_library_css(){
-//     wp_dequeue_style('wp-block-library');
-//     wp_dequeue_style('wp-block-library-theme');
-//     wp_dequeue_style('wc-block-style'); // If using WooCommerce and its blocks
-// }
-// add_action('wp_enqueue_scripts', 'remove_wp_block_library_css', 100);
+//liason avec le fichier ajax php
+include get_template_directory() . '/assets/inc/ajax.php';
 
 
 
@@ -109,6 +104,7 @@ function custom_image_sizes () {
     add_image_size('single-page-photo', 563, 844, true);
     add_image_size ('miniature', 81, 71, true);
     add_image_size ('photo-thumbnail', 546, 495, true);
+    add_image_size('banner', 1440, 962, true);
 }
 
 add_action( 'after_setup_theme', 'custom_image_sizes' );
