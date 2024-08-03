f8ceca65c6f0a8f461908341d3ec2d30ea95dd68
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-03 08:26:17 +0200

    ajout de fichiers renommés

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 // Link to the ajax.php file
-include get_template_directory() . '/assets/inc/ajax.php';
+include get_template_directory() . '/assets/inc/ajax-handler.php';
 
 // Styles and scripts
 function nataliemota_enqueue_scripts() {
@@ -8,12 +8,12 @@ function nataliemota_enqueue_scripts() {
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap', array(), null);
 
     wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js');
-    wp_enqueue_script('ajax-handler', get_template_directory_uri() . '/assets/js/ajax-handler.js', array('jquery'), null, true);
+    wp_enqueue_script('ajax-js', get_template_directory_uri() . '/assets/js/ajax-handler.js', array('jquery'), null, true);
 
     // Localize the script with new data
-    wp_localize_script('ajax-handler', 'myAjax', array(
+    wp_localize_script('ajax-js', 'myAjax', array(
         'ajax_url' => admin_url('admin-ajax.php'),
-        'nonce' => wp_create_nonce('natalie-mota-nonce')
+        'nonce' => wp_create_nonce('nm-nonce')
     ));
 }
 add_action('wp_enqueue_scripts', 'nataliemota_enqueue_scripts');
@@ -99,3 +99,33 @@ function custom_image_sizes() {
     add_image_size('banner', 1440, 962, true);
 }
 add_action('after_setup_theme', 'custom_image_sizes');
+
+//BANNER FUNCTION
+function random_hero_image_shortcode() {
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 1,
+        'orderby' => 'rand'
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+        }
+        wp_reset_postdata();
+    }
+
+    ob_start();
+    ?>
+    <div class="hero" style="background-image: url('<?php echo esc_url($image_url); ?>');">
+        <div class="hero-content">
+            <h1>PHOTOGRAPHE EVENT</h1>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('random_hero_image', 'random_hero_image_shortcode');
