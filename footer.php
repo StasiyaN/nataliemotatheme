3594c912c593962ec277b530112dc018405386ba
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-04 13:09:10 +0200

    creation du block lightbox dans le footer

--- footer.php
+++ footer.php
@@ -1,12 +1,29 @@
 
-    <div id="footer">
-      <?php
-        wp_nav_menu( array(
-            'theme_location' => 'footer',
-            'menu_id'        => 'footer-menu',
-        ) );
-        ?>
-    </div>
-    <?php wp_footer(); ?>
-</body>
+            <div id="footer">
+                <?php
+                wp_nav_menu( array(
+                    'theme_location' => 'footer',
+                    'menu_id'        => 'footer-menu',
+                ) );
+                ?>
+            </div>
+            <!-- CREATION DU LIGHTBOX STUCTURE -->
+                    <div id="lightbox-overlay" class="lightbox-overlay">
+                        <div class="lightbox-content">
+                            <span id="lightbox-close" class="lightbox-close"><i class="fa-solid fa-xmark"></i></span>
+                                <img id="lightbox-image" class="lightbox-image" src="" alt="">
+                                <div class="lightbox-info">
+                                    <div id="lightbox-ref" class="lightbox-ref"></div>
+                                    <div id="lightbox-cat" class="lightbox-cat"></div>
+                                    <snap id="lightbox-prev" class="lightbox-nav"><i class="fa-solid fa-arrow-left-long"></i>Précédent</span>
+                                    <span id="lightbox-next" class="lightbox-nav">Suivant<i class="fa-solid fa-arrow-right-long"></i></span>
+                                </div>
+                        </div>
+                    </div>
+
+
+
+
+        <?php wp_footer(); ?>
+    </body>
 </html>
