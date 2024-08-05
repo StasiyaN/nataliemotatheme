10505bad82a9b307071f1ed0192500379adadd1c
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-05 17:20:05 +0200

    stucture de lightbox css ok

--- footer.php
+++ footer.php
@@ -18,8 +18,8 @@
                                 <!-- <img class="lightbox-image" src="" alt=""> -->
                                 <img src="<?php echo get_template_directory_uri() . ('/assets/img/test.jpg'); ?>" alt="" class="">
                                     <div class="lightbox-info">
-                                        <div id="lightbox-ref" class="lightbox-ref">refernce</div>
-                                        <div id="lightbox-cat" class="lightbox-cat">categorie</div>
+                                        <div class="lightbox-reference">refernce</div>
+                                        <div class="lightbox-categorie">categorie</div>
                                     </div>
                                 </div>
                     </div>
