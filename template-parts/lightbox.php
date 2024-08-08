756c3c1b01d18a11ce85c97c896111cbc3b8c2bd
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-08 11:17:04 +0200

    mise en place lightbox js + suppression d'image de test

--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -4,8 +4,7 @@
               <span class="lightbox-next"><p>Suivant</p><i class="fa-solid fa-arrow-right-long"></i></span>
               <span class="lightbox-prev"><i class="fa-solid fa-arrow-left-long"></i><p>Précédent</p></span>
                   <div class="lightbox-container">
-                  <!-- <img class="lightbox-image" src="" alt=""> -->
-                  <img src="<?php echo get_template_directory_uri() . ('/assets/img/test.jpg'); ?>" alt="" class="">
+                  <img class="lightbox-image" src="" alt="">             
                       <div class="lightbox-info">
                           <div class="lightbox-reference">refernce</div>
                           <div class="lightbox-categorie">categorie</div>
