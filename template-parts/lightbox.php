023ac46d228e8ab101d9f8042c4e5f745879a326
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-08 15:13:00 +0200

    ajout ombre navbar

--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -1,8 +1,8 @@
 
       <div class="lightbox">
               <span class="lightbox-close"><i class="fa-solid fa-xmark"></i></span>
-              <span class="lightbox-next"><p>Suivant</p><i class="fa-solid fa-arrow-right-long"></i></span>
-              <span class="lightbox-prev"><i class="fa-solid fa-arrow-left-long"></i><p>Précédent</p></span>
+              <span class="lightbox-next"><p>Suivante</p><i class="fa-solid fa-arrow-right-long"></i></span>
+              <span class="lightbox-prev"><i class="fa-solid fa-arrow-left-long"></i><p>Précédente</p></span>
                   <div class="lightbox-container">
                   <img class="lightbox-image" src="" alt="">             
                       <div class="lightbox-info">
