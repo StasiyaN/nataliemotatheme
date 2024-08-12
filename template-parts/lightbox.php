7bf239b537f7fc7d15864919c3ffa41f46dd9387
StasiyaN <ana.nyzhalska@gmail.com>
2024-08-12 10:55:27 +0200

    ajustements adaptative + mise en place adaptative pour lightbox

--- template-parts/lightbox.php
+++ template-parts/lightbox.php
@@ -1,8 +1,8 @@
 
       <div class="lightbox">
               <span class="lightbox-close"><i class="fa-solid fa-xmark"></i></span>
-              <span class="lightbox-next"><p>Suivante</p><i class="fa-solid fa-arrow-right-long"></i></span>
-              <span class="lightbox-prev"><i class="fa-solid fa-arrow-left-long"></i><p>Précédente</p></span>
+              <span class="lightbox-next"><p class ="lightbox-txt">Suivante</p><i class="fa-solid fa-arrow-right-long"></i></span>
+              <span class="lightbox-prev"><i class="fa-solid fa-arrow-left-long"></i><p class="lightbox-txt">Précédente</p></span>
                   <div class="lightbox-container">
                   <img class="lightbox-image" src="" alt="">             
                       <div class="lightbox-info">
